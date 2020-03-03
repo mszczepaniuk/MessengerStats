4e7dcca853cb8c91e81896c37acd6279c3d16f59
mszczepaniuk <asd2ws@gmail.com>
2020-03-03 18:07:59 +0100

    Added methods responsible for deleting elements from collections in conversation

--- src/ApplicationCore/Entities/Conversation.cs
+++ src/ApplicationCore/Entities/Conversation.cs
@@ -10,5 +10,42 @@ namespace ApplicationCore.Entities
         public List<User> Users { get; set; }
         public List<Message> Messages { get; set; }
         public List<Reaction> Reactions { get; set; }
+
+        public void DeleteMessage(Message message)
+        {
+            Messages.Remove(message);
+            message.User.Messages.Remove(message);
+            foreach (var reaction in message.Reactions)
+            {
+                Reactions.Remove(reaction);
+                reaction.User.Reactions.Remove(reaction);
+            }
+        }
+
+        public void DeleteUser(User user)
+        {
+            Users.Remove(user);
+            foreach (var message in user.Messages)
+            {
+                Messages.Remove(message);
+                foreach (var reaction in message.Reactions)
+                {
+                    Reactions.Remove(reaction);
+                    reaction.User.Reactions.Remove(reaction);
+                }
+            }
+            foreach (var reaction in user.Reactions)
+            {
+                Reactions.Remove(reaction);
+                reaction.Message.Reactions.Remove(reaction);
+            }
+        }
+
+        public void DeleteReaction(Reaction reaction)
+        {
+            Reactions.Remove(reaction);
+            reaction.User.Reactions.Remove(reaction);
+            reaction.Message.Reactions.Remove(reaction);
+        }
     }
 }
---
+++ test/UnitTests/ApplicationCore/Entities/ConversationTests.cs
@@ -0,0 +1,116 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace UnitTests.ApplicationCore.Entities
+{
+    public class ConversationTests
+    {
+        private Conversation stubConversation;
+
+        public ConversationTests()
+        {
+            stubConversation = CreateStubConversation();
+        }
+
+        [Fact]
+        public void DeleteReaction_IsNotPresentInMessageReactions()
+        {
+            var reaction = stubConversation.Reactions.FirstOrDefault();
+            stubConversation.DeleteReaction(reaction);
+
+            Assert.DoesNotContain(reaction, reaction.Message.Reactions);
+        }
+
+        [Fact]
+        public void DeleteReaction_IsNotPresentInUserReactions()
+        {
+            var reaction = stubConversation.Reactions.FirstOrDefault();
+            stubConversation.DeleteReaction(reaction);
+
+            Assert.DoesNotContain(reaction, reaction.User.Reactions);
+        }
+
+        [Fact]
+        public void DeleteMessage_IsNotPresentInUserMessagges()
+        {
+            var message = stubConversation.Messages.FirstOrDefault();
+            stubConversation.DeleteMessage(message);
+
+            Assert.DoesNotContain(message, message.User.Messages);
+        }
+
+        [Fact]
+        public void DeleteMessage_SumOfReferencesIsCorrect()
+        {
+            var message = stubConversation.Messages.FirstOrDefault();
+            stubConversation.DeleteMessage(message);
+
+            int userMessages = 0;
+            foreach (var tempUser in stubConversation.Users)
+            {
+                userMessages += tempUser.Messages.Count;
+            }
+            Assert.Equal(stubConversation.Messages.Count, userMessages);
+        }
+
+        [Fact]
+        public void DeleteUser_SumOfReferencesIsCorrect()
+        {
+            var user = stubConversation.Users.FirstOrDefault();
+            stubConversation.DeleteUser(user);
+
+            int userMessages = 0;
+            foreach (var tempUser in stubConversation.Users)
+            {
+                userMessages += tempUser.Messages.Count;
+            }
+            Assert.Equal(stubConversation.Messages.Count, userMessages);
+
+            int userReactions = 0;
+            foreach (var tempUser in stubConversation.Users)
+            {
+                userReactions += tempUser.Reactions.Count;
+            }
+            Assert.Equal(stubConversation.Reactions.Count, userReactions);
+        }
+
+        private Conversation CreateStubConversation()
+        {
+            var conversation = new Conversation { Users = new List<User>(), Messages = new List<Message>(), Reactions = new List<Reaction>() };
+            var user1 = new User { Id = 0, Name = "Andrzej", Messages = new List<Message>(), Reactions = new List<Reaction>() };
+            var user2 = new User { Id = 1, Name = "Marcin", Messages = new List<Message>(), Reactions = new List<Reaction>() };
+            var user3 = new User { Id = 2, Name = "Filip", Messages = new List<Message>(), Reactions = new List<Reaction>() };
+            conversation.Users.AddRange(new List<User> { user1, user2, user3 });
+            foreach (var user in conversation.Users)
+            {
+                var message = new Message { User = user, Content = "123", CreationDate = DateTime.Now, Reactions = new List<Reaction>() };
+                conversation.Messages.Add(message);
+                user.Messages.Add(message);
+                message = new Message { User = user, Content = "123", CreationDate = DateTime.Now, Reactions = new List<Reaction>() };
+                conversation.Messages.Add(message);
+                user.Messages.Add(message);
+            }
+
+            var reaction = new Reaction { Message = conversation.Messages[0], ReactionType = ReactionType.Angry, User = user2 };
+            conversation.Reactions.Add(reaction);
+            user2.Reactions.Add(reaction);
+            conversation.Messages[0].Reactions.Add(reaction);
+
+            reaction = new Reaction { Message = conversation.Messages[3], ReactionType = ReactionType.Angry, User = user1 };
+            conversation.Reactions.Add(reaction);
+            user1.Reactions.Add(reaction);
+            conversation.Messages[3].Reactions.Add(reaction);
+
+            reaction = new Reaction { Message = conversation.Messages[5], ReactionType = ReactionType.Angry, User = user3 };
+            conversation.Reactions.Add(reaction);
+            user1.Reactions.Add(reaction);
+            conversation.Messages[5].Reactions.Add(reaction);
+
+            return conversation;
+        }
+    }
+}
